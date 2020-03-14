c7c4e43e65e340c75c046614df79475c5551436f
bynl-44 <xyxcnnh@icloud.com>
2020-03-14 22:29:55 +0800

    inital commit

---
+++ blog/2020-03-14-hello-world.md
@@ -0,0 +1,15 @@
+---
+id: hello-world
+title: Hello
+author: ican
+author_title:
+author_url:
+author_image_url: https://avatars3.githubusercontent.com/u/20472178?s=400&u=82a544942743b8aa6f4befa3481e2ad24d9bff7b&v=4
+tags: [hello, docusaurus]
+---
+
+Welcome!
+
+<!-- truncate -->
+
+这只是一个测试提交.
---
+++ docusaurus.config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  title: "ican",
+  tagline: "The tagline of my site",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "ican", // Usually your GitHub org/user name.
+  projectName: "Dayup", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "ican",
+      logo: {
+        alt: "Dayup",
+        src: "img/logo.svg"
+      },
+      links: [
+        // {
+        //   to: "docs/doc1",
+        //   activeBasePath: "docs",
+        //   label: "Docs",
+        //   position: "left"
+        // },
+        { to: "/", label: "Blog", position: "left" }
+        // {
+        //   href: "https://github.com/facebook/docusaurus",
+        //   label: "GitHub",
+        //   position: "right"
+        // }
+      ]
+    },
+    footer: {
+      style: "dark",
+      links: [
+        // {
+        //   title: "Docs",
+        //   items: [
+        //     {
+        //       label: "Style Guide",
+        //       to: "docs/doc1"
+        //     },
+        //     {
+        //       label: "Second Doc",
+        //       to: "docs/doc2"
+        //     }
+        //   ]
+        // },
+        // {
+        //   title: "Community",
+        //   items: [
+        //     {
+        //       label: "Stack Overflow",
+        //       href: "https://stackoverflow.com/questions/tagged/docusaurus"
+        //     },
+        //     {
+        //       label: "Discord",
+        //       href: "https://discordapp.com/invite/docusaurus"
+        //     }
+        //   ]
+        // },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              to: "blog"
+            }
+            // {
+            //   label: "GitHub",
+            //   href: "https://github.com/facebook/docusaurus"
+            // },
+            // {
+            //   label: "Twitter",
+            //   href: "https://twitter.com/docusaurus"
+            // }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Dayup, Inc. Built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        // docs: {
+        //   sidebarPath: require.resolve("./sidebars.js"),
+        //   editUrl: "https://github.com/facebook/docusaurus/edit/master/website/"
+        // },
+        blog: {
+          path: "./blog",
+          routeBasePath: "/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
+};
